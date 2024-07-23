3a49783ba79fbae8b5d5d164f09053775a63b368
foash-111 <mr2221@fayoum.edu.eg>
2024-07-23 03:31:33 +0300

    shorthand Property Names in ES6 JavaScript

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,4 +1,4 @@
-//  You can simplify the function by using the shorthand property names feature in ES6,
+// You can simplify the function by using the shorthand property names feature in ES6,
 // which allows you to use just the variable names if they match the property names. Here's the modified function:
 export default function getBudgetObject (income, gdp, capita) {
   const budget = {
