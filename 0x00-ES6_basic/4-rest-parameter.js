3f2f70d38480fd50f67020305eb063f44906f159
foash-111 <mr2221@fayoum.edu.eg>
2024-07-23 03:27:31 +0300

    rest parameters

---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,8 @@
+export default function returnHowManyArguments (...args) {
+//     let counter = 0
+//     for (const arg of args) {
+//         counter++;
+//     }
+// return counter
+  return args.length;
+}
