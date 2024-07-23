244f0377c3a0109c8c363c4197b414d84ef9b77c
foash-111 <mr2221@fayoum.edu.eg>
2024-07-23 16:12:44 +0300

    return concated string

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,5 +1,5 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let newString = "";
+  let newString = '';
   for (const value of reportWithIterator) {
     if (value === reportWithIterator.at(-1)) {
       newString += value;
