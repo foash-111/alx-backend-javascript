3d05b609cc7579ea2335c9bf5f35c6df205fc1ef
foash-111 <mr2221@fayoum.edu.eg>
2024-07-25 03:06:33 +0300

    promise.then(arrow func print msg)

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  const myPromise = new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({
-        status: 200,
-        body: 'success',
-      });
-    } else {
-      reject(new Error());
-    }
-  }).finally(() => console.log('Got a response from the API'));
-  return myPromise;
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
