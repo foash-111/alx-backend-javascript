5c898ebcd5495b684ba6e7e49576cf42443461f7
foash-111 <mr2221@fayoum.edu.eg>
2024-07-26 05:41:11 +0300

    getter and setter and handling errors using throw error

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,5 +1,5 @@
 export default class Currency {
-  constructor (code = '', name = '') {
+  constructor(code = '', name = '') {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a String');
     } else {
@@ -12,15 +12,15 @@ export default class Currency {
     }
   }
 
-  get name () {
+  get name() {
     return this._name;
   }
 
-  get code () {
+  get code() {
     return this._code;
   }
 
-  set name (value) {
+  set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a String');
     } else {
@@ -28,7 +28,7 @@ export default class Currency {
     }
   }
 
-  set code (value) {
+  set code(value) {
     if (typeof code !== 'string') {
       throw new TypeError('Code must be a String');
     } else {
@@ -36,7 +36,7 @@ export default class Currency {
     }
   }
 
-  displayFullCurrency () {
+  displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
 }
