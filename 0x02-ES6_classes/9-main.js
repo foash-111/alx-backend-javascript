cfd9d19ca9773a1281040d8ec316321e4b86aa13
foash-111 <mr2221@fayoum.edu.eg>
2024-07-28 18:06:38 +0300

    default export at the end and no inheritance

--- 0x02-ES6_classes/9-main.js
+++ 0x02-ES6_classes/9-main.js
@@ -1,7 +1,10 @@
-import listOfStudents from "./9-hoisting.js";
+import listOfStudents from "./9-hoisting";
 
 console.log(listOfStudents);
 
 const listPrinted = listOfStudents.map(
     student => student.fullStudentDescription
 );
+
+
+console.log(listPrinted)
