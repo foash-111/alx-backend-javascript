9ef8fd977e455e2c5ebc8665d36fca84bfe06c3d
foash-111 <mr2221@fayoum.edu.eg>
2024-07-29 12:23:07 +0300

    use map function

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+export default function getListStudentIds(value) {
+    if (Array.isArray(value)) {
+        const newArray = value.map(x => x['id']);
+        return newArray;
+    }
+    return [];
+}
