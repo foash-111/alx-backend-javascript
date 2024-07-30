33c7982db5c6ced1dec6b9b5ebd0b18dce119f7e
foash-111 <mr2221@fayoum.edu.eg>
2024-07-30 12:04:53 +0300

    reduce function

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,14 +1,13 @@
-
-import getListStudentIds from './1-get_list_student_ids';
+import getListStudentIds from './1-get_list_student_ids'
 
 export default function getStudentIdsSum (wholeList = []) {
   if (Array.isArray(wholeList)) {
-    const idList = getListStudentIds(wholeList);
+    const idList = getListStudentIds(wholeList)
     return idList.reduce((accumulator, current) => {
-      return accumulator + current;
+      return accumulator + current
     }
-    , 0);
+    , 0)
   }
 
-  return [];
+  return []
 }
