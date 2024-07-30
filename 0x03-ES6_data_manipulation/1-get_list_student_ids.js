4b7a3a9e57b678d6e789c340ae5aba300ffc1fed
foash-111 <mr2221@fayoum.edu.eg>
2024-07-30 12:03:57 +0300

    map function

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
-export default function getListStudentIds(value) {
-    if (Array.isArray(value)) {
-        const newArray = value.map(x => x['id']);
-        return newArray;
-    }
-    return [];
+export default function getListStudentIds (value) {
+  if (Array.isArray(value)) {
+    const newArray = value.map(x => x.id)
+    return newArray
+  }
+  return []
 }
--- 0x03-ES6_data_manipulation/100-main.js
+++ 0x03-ES6_data_manipulation/100-main.js
@@ -1,15 +1,15 @@
-import { queryAPI, weakMap } from "./100-weak.js";
+import { queryAPI, weakMap } from './100-weak.js'
 
-const endpoint = { protocol: 'http', name: 'getUsers' };
-weakMap.get(endpoint);
+const endpoint = { protocol: 'http', name: 'getUsers' }
+weakMap.get(endpoint)
 
-queryAPI(endpoint);
-console.log(weakMap.get(endpoint));
+queryAPI(endpoint)
+console.log(weakMap.get(endpoint))
 
-queryAPI(endpoint);
-console.log(weakMap.get(endpoint));
+queryAPI(endpoint)
+console.log(weakMap.get(endpoint))
 
-queryAPI(endpoint);
-queryAPI(endpoint);
-queryAPI(endpoint);
-queryAPI(endpoint);
+queryAPI(endpoint)
+queryAPI(endpoint)
+queryAPI(endpoint)
+queryAPI(endpoint)
