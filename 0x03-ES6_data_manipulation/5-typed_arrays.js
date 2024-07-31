c40609dfeaf4ca78732872e19511ebfbcd2d144b
foash-111 <mr2221@fayoum.edu.eg>
2024-07-31 04:22:32 +0300

    typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,14 @@
 export default function createInt8TypedArray(length = 0, position = 0, value = 0) {
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
   try {
     const buffer = new ArrayBuffer(length);
     const dataView = new DataView(buffer);
     dataView.setInt8(position, value);
     return dataView;
   } catch (e) {
-    console.error('Position outside range');
+    console.error(e);
   }
-  return 'Position outside range';
+  return null;
 }
